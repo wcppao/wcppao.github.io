73339b20c5979a1198bccd53675f13efe1ff6ea7
wcppao <165740295+wcppao@users.noreply.github.com>
2025-12-17 14:29:52 +0800

    Update image and video entries in config.js

--- js/config.js
+++ js/config.js
@@ -9,22 +9,22 @@ const config = {
         { src: "/image/album/life/photo5.jpg", alt: "生活照片5", tag: "life" },
         { src: "/image/album/life/photo6.jpg", alt: "生活照片6", tag: "life" },
         { src: "/image/album/life/photo7.jpg", alt: "生活照片7", tag: "life" },
-        { src: "/image/album/life/photo8.jpg", alt: "生活照片1", tag: "life" },
-        { src: "/image/album/life/photo9.jpg", alt: "生活照片2", tag: "life" },
-        { src: "/image/album/life/photo10.jpg", alt: "生活照片3", tag: "life" },
-        { src: "/image/album/life/photo11.jpg", alt: "生活照片4", tag: "life" },
-        { src: "/image/album/life/photo12.jpg", alt: "生活照片5", tag: "life" },
-        { src: "/image/album/life/photo13.jpg", alt: "生活照片6", tag: "life" },
-        { src: "/image/album/life/photo14.jpg", alt: "生活照片7", tag: "life" },
-        { src: "/image/album/life/photo15.jpg", alt: "生活照片1", tag: "life" },
-        { src: "/image/album/life/photo16.jpg", alt: "生活照片2", tag: "life" },
-        { src: "/image/album/life/photo17.jpg", alt: "生活照片3", tag: "life" },
-        { src: "/image/album/life/photo18.jpg", alt: "生活照片4", tag: "life" },
+        { src: "/image/album/life/photo8.jpg", alt: "生活照片8", tag: "life" },
+        { src: "/image/album/life/photo9.jpg", alt: "生活照片9", tag: "life" },
+        { src: "/image/album/life/photo10.jpg", alt: "生活照片10", tag: "life" },
+        { src: "/image/album/life/photo11.jpg", alt: "生活照片11", tag: "life" },
+        { src: "/image/album/life/photo12.jpg", alt: "生活照片12", tag: "life" },
+        { src: "/image/album/life/photo13.jpg", alt: "生活照片13", tag: "life" },
+        { src: "/image/album/life/photo14.jpg", alt: "生活照片14", tag: "life" },
+        { src: "/image/album/life/photo15.jpg", alt: "生活照片15", tag: "life" },
+        { src: "/image/album/life/photo16.jpg", alt: "生活照片16", tag: "life" },
+        { src: "/image/album/life/photo17.jpg", alt: "生活照片17", tag: "life" },
+        { src: "/image/album/life/photo18.jpg", alt: "生活照片18", tag: "life" }
         // 可以继续添加更多图片，按照photoN.jpg的格式命名
     ],
     videos: [
         { src: "/image/album/life/video1.mp4", alt: "生活视频1", tag: "life", thumbnail: "/image/album/life/thumbnail1.jpg" },
-        { src: "/image/album/life/video2.mp4", alt: "生活视频2", tag: "life", thumbnail: "/image/album/life/thumbnail2.jpg" },
+        { src: "/image/album/life/video2.mp4", alt: "生活视频2", tag: "life", thumbnail: "/image/album/life/thumbnail2.jpg" }
         // 可以继续添加更多视频
     ]
 };
