bf4d0f99ec8a6df1296365bdc32163ffa7320de9
wcppao <165740295+wcppao@users.noreply.github.com>
2025-12-17 14:09:01 +0800

    Add album configuration for images and videos
    Added configuration for images and videos with metadata.

--- js/config.js
+++ js/config.js
@@ -1,1 +1,19 @@
-
+// 相册配置信息
+const config = {
+    itemsPerPage: 6, // 每页显示的项目数
+    images: [
+        { src: "/image/album/life/photo1.jpg", alt: "生活照片1", tag: "life" },
+        { src: "/image/album/life/photo2.jpg", alt: "生活照片2", tag: "life" },
+        { src: "/image/album/life/photo3.jpg", alt: "生活照片3", tag: "life" },
+        { src: "/image/album/life/photo4.jpg", alt: "生活照片4", tag: "life" },
+        { src: "/image/album/life/photo5.jpg", alt: "生活照片5", tag: "life" },
+        { src: "/image/album/life/photo6.jpg", alt: "生活照片6", tag: "life" },
+        { src: "/image/album/life/photo7.jpg", alt: "生活照片7", tag: "life" },
+        // 可以继续添加更多图片，按照photoN.jpg的格式命名
+    ],
+    videos: [
+        { src: "/image/album/life/video1.mp4", alt: "生活视频1", tag: "life", thumbnail: "/image/album/life/thumbnail1.jpg" },
+        { src: "/image/album/life/video2.mp4", alt: "生活视频2", tag: "life", thumbnail: "/image/album/life/thumbnail2.jpg" },
+        // 可以继续添加更多视频
+    ]
+};
